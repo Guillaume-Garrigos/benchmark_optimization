31d2f979c3efb57148b652beb90a12b29193b23b
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2022-05-20 22:55:06 +0200

    Moved import_all_classes to utils

--- src/solvers/__init__.py
+++ src/solvers/__init__.py
@@ -1,25 +1,4 @@
-from pathlib import Path
-
-def is_forbidden_filename(string):
-    return (string.startswith("_") # ignore __init__ or others
-            or (string in ["solver", "records"]) # those contain the definition of the classes
-            or string.endswith("-checkpoint") ) # those can appear with jupyterlab in .ipynb_checkpoints
-
-def is_forbidden_foldername(string):
-    return (string.startswith("_") # ignore __pycache__ or others
-            or string.startswith(".")) # ignore .ipynb_checkpoints or others
-
-def import_all_classes(folder_path, class_name):
-    """ Import all the classes of type class_name
-        that can be found within folder_path
-        There is no recursion here, we only look at files like folder_path/file_name.py
-    """
-    for file in Path(folder_path).glob(r"**/*.py"): # any .py file in any subfolder
-        file_name = file.stem # the name of the file without folder or extension
-        if not is_forbidden_filename(file_name): 
-            # file > remove extension > replace / with .
-            file_import_path = ".".join(part for part in file.with_suffix('').parts) 
-            __import__(file_import_path, fromlist=[class_name])
+from src.utils import import_all_classes
             
 # the code below imports all the subclasses of Solver() defined in the /solvers folder, possibly accross differnt files
 # once this is done, they all exist for the python interpreter
--- src/utils.py
+++ src/utils.py
@@ -47,3 +47,29 @@ def f_val_logistic(x, data, label, loss, regularizer, reg):
 
 def f_grad_logistic(x, data, label, loss, regularizer, reg):
     return np.mean(loss.prime(label, data @ x).reshape(-1, 1) * data, axis=0) + reg * regularizer.prime(x)
+
+# ======================
+# Functions to import classes without listing them
+# ======================
+
+from pathlib import Path
+
+def is_forbidden_filename(string):
+    return (string.startswith("_") # ignore __init__ or others
+            or (string in ["solver", "records"]) # those contain the definition of the classes
+            or string.endswith("-checkpoint") ) # those can appear with jupyterlab in .ipynb_checkpoints
+
+def is_forbidden_foldername(string):
+    return (string.startswith("_") # ignore __pycache__ or others
+            or string.startswith(".")) # ignore .ipynb_checkpoints or others
+
+def import_all_classes(folder_path, class_name):
+    """ Import all the classes of type class_name
+        that can be found within folder_path
+    """
+    for file in Path(folder_path).glob(r"**/*.py"): # any .py file in any subfolder
+        file_name = file.stem # the name of the file without folder or extension
+        if not is_forbidden_filename(file_name): 
+            # file > remove extension > replace / with .
+            file_import_path = ".".join(part for part in file.with_suffix('').parts) 
+            __import__(file_import_path, fromlist=[class_name])
