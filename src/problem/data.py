9a9e37b5c4b1147b5e597ef9bbccc5e8c03acbf6
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2022-11-18 18:11:29 +0100

    Refactor code and add phase retrieval problem

--- src/problem/data.py
+++ src/problem/data.py
@@ -21,16 +21,40 @@ class Data():
         self.path = ""
         self.nb_data = None
         self.dim = None
+        self.config = get_config()
     
-    def read(self, dataset_name, dataset_path_folder):
-        # Given data (=features and labels) stored somewhere, loads it
-        # must be stored in a file dataset_path_folder/dataset_name.txt
+    def read(self, dataset_name, dataset_path_folder=None):
+        # Obtain the data. Either loaded or generated
         self.name = dataset_name
-        self.path = os.path.join(dataset_path_folder, dataset_name) + '.txt'
-        X, y = get_data(self.path)
-        X = X.toarray()  # convert from scipy sparse matrix to dense if necessary
+        if dataset_name == "random":
+            X, y = self.generate_data()
+        else:
+            self.path = os.path.join(dataset_path_folder, dataset_name) + '.txt'
+            X, y = load_data(self.path)
+            X = X.toarray()  # convert from scipy sparse matrix to dense if necessary
+        X, y = self.preprocess_data(X, y)
+        n, d = X.shape
+        logging.info("Number of data points: {:d}; Number of features: {:d}".format(n, d))
+        self.feature = X
+        self.label = y
+        self.nb_data = n
+        self.dim = d
+    
+    def generate_data(self):
+        problem_type = self.config['problem']['type']
+        nb_data = 100
+        nb_features = 50
+        std = 0.0
+        if problem_type == 'phase retrieval':
+            x = np.random.randn(nb_features)
+            A = np.random.randn(nb_data, nb_features)
+            noise = std* np.random.randn(nb_data)
+            b = (A @ x)**2 + noise
+        return A, b
+
+    def preprocess_data(self, X, y):
         logging.info("Data Sparsity: {}".format(sparsity(X)))
-        problem_type = get_config()['problem']['type']
+        problem_type = self.config['problem']['type']
         if problem_type == 'classification':
             # label preprocessing for some dataset whose label is not {-1, 1}
             max_v, min_v = np.max(y), np.min(y)
@@ -38,22 +62,17 @@ class Data():
             idx_max = (y == max_v)
             y[idx_min] = -1
             y[idx_max] = 1
-        elif problem_type == 'regression':
+        elif problem_type in ['regression','phase retrieval'] :
             X = X / np.max(np.abs(X), axis=0)  # scale features to [-1, 1]
         else:
             raise Exception("Unknown problem type!")
         # adding a column filling with all ones.(bias term)
         X = np.c_[X, np.ones(X.shape[0])]
+        return X, y
 
-        n, d = X.shape
-        logging.info("Number of data points: {:d}; Number of features: {:d}".format(n, d))
-        self.feature = X
-        self.label = y
-        self.nb_data = n
-        self.dim = d
 ##################################################
 
-def get_data(data_path):
+def load_data(data_path):
     """Once datasets are downloaded, load datasets in LibSVM format."""
     data = load_svmlight_file(data_path)
     return data[0], data[1]
@@ -68,8 +87,10 @@ def sparsity(data):
     return zeros_entries / total_entries
 
 # ===============================
-# These two functions to generate artificial data. Not used.
+# These functions to generate artificial data. Not used?
 # =================================
+
+
 
 def simu_linreg(x, n, std=1., corr=0.5):
     """Simulation for the least-squares problem.
@@ -99,54 +120,5 @@ def simu_linreg(x, n, std=1., corr=0.5):
     b = A.dot(x) + noise
     return A, b
 
-
-def simu_logreg(x, n, std=1., corr=0.5):
-    """Simulation for the logistic regression problem.
-
-    Parameters
-    ----------
-    x : ndarray, shape (d,)
-        The coefficients of the model
-    n : int
-        Sample size
-    std : float, default=1.
-        Standard-deviation of the noise
-    corr : float, default=0.5
-        Correlation of the features matrix
-
-    Returns
-    -------
-    A : ndarray, shape (n, d)
-        The design matrix.
-    b : ndarray, shape (n,)
-        The targets.
-    """
-    A, b = simu_linreg(x, n, std=1., corr=corr)
-    return A, np.sign(b)
-
-def simu_phase_retrieval(x, n, std=0.):
-    """Simulation for phase retrieval problem.
-
-    Parameters
-    ----------
-    x : ndarray, shape (d,)
-        The coefficients of the model
-    n : int
-        Sample size
-
-    Returns
-    -------
-    A : ndarray, shape (n, d)
-        The design matrix.
-    b : ndarray, shape (n,)
-        The targets.
-    std : float, default=1.
-        Standard-deviation of the noise
-    """
-    d = x.shape[0]
-    A = randn(n, d)
-    noise = std* randn(n)
-    b = (A.dot(x))**2 + noise
-    return A, b
 
 # ======================================
