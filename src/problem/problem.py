eab29d8e641f598ab351649b57549328618d859e
Guillaume Garrigos <guillaume.garrigos@lpsm.paris>
2023-02-14 14:30:23 +0100

    New: we can save and load exact solutions found via bfgs

--- src/problem/problem.py
+++ src/problem/problem.py
@@ -1,5 +1,8 @@
 import numpy as np
 import logging
+import os
+import pickle
+from datetime import datetime
 
 from src.parameters import Parameters
 import src.problem.loss as loss
@@ -26,6 +29,7 @@ class Problem():
         # parameters
         self.param = Parameters(data)
         config = self.param.config
+        self.type = config['problem']['type']
         # data
         self.data = data
         self.nb_data = data.nb_data
@@ -58,10 +62,15 @@ class Problem():
             if config['problem']['reg_parameter'] == "inv_sqrt_data":
                 self.reg_parameter = 1./np.sqrt(self.nb_data)
         logging.info("Regularization param: {}".format(self.reg_parameter))
-        
+        # summary
+        self.summary = {'type' : self.type, 
+                        'loss' : self.loss_name, 
+                        'regularizer' : self.regularizer_name, 
+                        'reg_parameter' : self.reg_parameter, 
+                        'dataset_name' : self.data.name }
         # solution of the problem
         self.we_know_solution = False
-        if config['results']['subopt']:
+        if config['problem']['save_solution'] or config['results']['use_solution'] or config['results']['subopt']: # subopt is legacy for use_solution
             self.solve()
 
     # Nice handles 
@@ -93,33 +102,95 @@ class Problem():
         h = np.sqrt(self.loss.dprime(self.data.label, self.data.feature @ x)).reshape(-1, 1) * self.data.feature
         return (h.T @ h)/self.nb_data + self.reg_parameter * self.regularizer.dprime(x)
         
-
     def solve(self):
-        # try to obtain the optimum by using l-bfgs method
-        # but only the L2 logistic regression is supported so far
-        f_opt = 0.0
-        optimum = None
-        #self.optimal_value = None
+        # gets the "exact" solution of the problem
+        solution = {}
+        if self.param.config['problem']['load_solution']:
+            solution = self.load_solution()
+        if solution == {}: # load failed or we don't want to load
+            solution = self.compute_solution()
+        # we check the solution is good
+        g_0 = self.gradient(np.zeros(self.dim))
+        if solution['gradient_norm'] < 1e-5*np.sqrt(g_0 @ g_0):
+            pass
+        else: # failure
+            print("The gradient at given optimum is relatively larger than 1e-5, we think it is not an optimum")
+            solution = {}
+    
+        # store it in the Problem class
+        if solution != {}: 
+            self.we_know_solution = True
+            self.solution_info = solution['solver_info']
+            self.optimal_solution = solution['minimizer']
+            self.optimal_value = solution['infimum']
+            self.optimal_value_sampled = [self.function_sampled(solution['minimizer'], i) for i in range(self.nb_data)]
+            self.optimal_gradient_sqnorm_sampled = [np.linalg.norm(self.gradient_sampled(solution['minimizer'], i))**2 for i in range(self.nb_data)]
+            # save the solution if needed
+            if self.param.config['problem'].get('save_solution', False):
+                self.save_solution(solution)
+        return
+    
+    def problem_file_name(self):
+        # Given a problem,
+        # returns a unique string "characterizing" the problem
+        # dataset name + problem type + loss name + regularizer name
+        # we don't add the regularization parameter which could be messy
+        return self.data.name +'-'+ self.type +'-'+ self.loss_name +'-'+ self.regularizer_name
+    
+    def save_solution(self, solution):
+        timestamp = datetime.today().strftime('%y%m%d_%H%M%S')
+        solution['timestamp'] = timestamp
+        solution['signature'] = 'solution_solver'
+        folder = self.param.config['problem']['solution_path_folder']
+        if not os.path.exists(folder): 
+            os.makedirs(folder)
+        filename = self.problem_file_name() +'-'+ timestamp
+        path = os.path.join(folder, filename)
+        with open(path, 'wb') as file:
+            pickle.dump(solution, file)
+        return
+    
+    def compute_solution(self):
+        # try to compute the exact solution of the problem
+        # returns a dict with all the information if computation is successful
+        # return empty dict otherwise
         if self.loss_name == "Logistic" and self.regularizer_name == "L2":
             print("Solve the problem with scipy fmin_l_bfgs_b")
-            optimum, f_opt, d_info = fmin_l_bfgs_b(
-                func=utils.f_val_logistic, x0=np.zeros(self.dim), fprime=utils.f_grad_logistic,
-                args=(self.data.feature, self.data.label, self.loss, self.regularizer, self.reg_parameter), pgtol=1e-07)
-
-            # verify that the gradient at optimum is close to zero
-            g_opt = d_info['grad']
-            g_0 = self.gradient(np.zeros(self.dim))
-            if np.sqrt((g_opt @ g_opt)) > 1e-5*np.sqrt(g_0 @ g_0):
-                print("The gradient at given optimum is relatively larger than 1e-5, we think it is not an optimum")
-                # here self.we_know_solution remains equal to False
-                # this is what should be tested later in the code
-            else: # its ok
-                self.we_know_solution = True
-                self.optimal_solution = optimum
-                self.optimal_value = f_opt
-                self.optimal_value_sampled = [self.function_sampled(optimum, i) for i in range(self.nb_data)]
-                self.optimal_gradient_sqnorm_sampled = [np.linalg.norm(self.gradient_sampled(optimum, i))**2 for i in range(self.nb_data)]
-        return
+            # TODO review this bc i don't understand why we complicate things with utils.stuff... Maybe inherited from Jiabin?
+            pgtol = self.param.config['problem']['pgtol']
+            factr = self.param.config['problem']['factr']
+            optimum, f_opt, info = fmin_l_bfgs_b(
+                func=utils.f_val_logistic, 
+                x0=np.zeros(self.dim), 
+                fprime=utils.f_grad_logistic,
+                args=(self.data.feature, self.data.label, self.loss, self.regularizer, self.reg_parameter), 
+                pgtol = pgtol,
+                factr = factr)
+            # we store the results
+            solution = {'minimizer' : optimum, 
+                        'infimum' : f_opt, 
+                        'gradient_norm' : np.linalg.norm(info.pop('grad')), # pop deletes 'grad' after use, saving memory 
+                        'solver_info' : {**info, 'pgtol' : pgtol, 'factr' : factr}, # see https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fmin_l_bfgs_b.html#scipy.optimize.fmin_l_bfgs_b
+                        'problem' : self.summary }
+            return solution
+        else: # TODO
+            print("The problem can't be solved (not yet implemented)")
+            return {}
+            
+    
+    def load_solution(self):
+        # if it exists, load the solution of the problem at hand
+        # returns a dict (contains the solution and stuff) or an empty dict (if no load)
+        folder = self.param.config['problem']['solution_path_folder']
+        if os.path.exists(folder):
+            for file in os.scandir(folder):
+                with open(file.path, 'rb') as file:
+                    solution = pickle.load(file)
+                    if isinstance(solution, dict) and solution.get('signature') == 'solution_solver': # just in case there are other files in there
+                        # is this the solution of our problem at hand?
+                        if solution['problem'] == self.summary:
+                            return solution
+        return {}
     
     def get_gradient_variance(self, batch_size=1):
         if batch_size == 1:
